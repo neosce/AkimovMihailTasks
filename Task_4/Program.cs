a3abf0be75efc15981975fe01822ccdfc25363fd
AkimovMO <akimovmi95@gmail.com>
2019-12-11 23:08:08 +0400

    Add Task 4.1 Custom Sort

---
+++ Task_4/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Task_4.Tasks._4._1_Custom_Sort;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n;
+
+            Console.WriteLine("Task_4\n" + "The task corresponds to the number in the list:");
+
+            do
+            {
+                Console.WriteLine("1 - CUSTOM SORT\n" + "2 - CUSTOM SORT DEMO\n" + "3 - SORTING UNIT\n" + "4 - NUMBER ARRAY SUM\n" + "5 - TO INT OR NOT TO INT?\n" + "6 - I SEEK YOU*\n" + "0 - EXIT\n");
+
+                var number_list = Console.ReadLine();
+
+                if (int.TryParse(number_list, out n) && n < 4 && n >= 0)
+                {
+                    switch (n)
+                    {
+                        case 1:
+                            Custom_Sort.CustomSortMain();
+                            break;
+                        case 2:
+                            Console.WriteLine("");
+                            break;
+                        case 3:
+                            Console.WriteLine("");
+                            break;
+                        case 4:
+                            Console.WriteLine("");
+                            break;
+                        case 5:
+                            Console.WriteLine("");
+                            break;
+                        case 6:
+                            Console.WriteLine("");
+                            break;
+                        default:
+                            n = 0;
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Try once more...");
+                }
+
+                Console.WriteLine();
+
+            } while (n != 0);
+        }
+    }
+}
---
+++ Task_4/Tasks/4.1 Custom Sort/Custom_Sort.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace Task_4.Tasks._4._1_Custom_Sort
+{
+    public delegate void Swap(int[] items, int i, int j);
+
+    public class Custom_Sort
+    {
+        private static void SwapItems(int[] items, int i, int j)
+        {
+            int temp;
+            temp = items[i];
+            items[i] = items[j];
+            items[j] = temp;
+        }
+
+        private void BubbleSort(int[] items)
+        {
+            Swap swap = new Swap(SwapItems);
+
+            for (int i = 0; i < items.Length - 1; i++)
+            {
+                for (int j = i + 1; j < items.Length; j++)
+                {
+                    if (items[i] > items[j])
+                    {
+                        swap(items, i, j);
+                    }
+                }
+            }
+        }
+
+        private void ArrayRand(int[] items, int elements)
+        {
+            Random rnd = new Random();
+
+            Console.Write("Random array: ");
+            Console.Write("{ ");
+            for (int i = 0; i < elements; i++)
+            {
+                items[i] = rnd.Next(0, 100);
+                Console.Write(items[i]);
+                Console.Write(",");
+            }
+            Console.Write("}");
+        }
+
+        private void Print_Array(int[] items)
+        {
+
+            Console.Write("\nSort array: ");
+            Console.Write("{ ");
+            for (int i = 0; i < items.Length; i++)
+            {
+                Console.Write(items[i]);
+                Console.Write(",");
+            }
+            Console.Write("}\n");
+        }
+
+        public static void CustomSortMain()
+        {
+            Console.WriteLine("Custom Sotr".ToUpper());
+
+            Custom_Sort custom_Sort = new Custom_Sort();
+            Random rndArray = new Random();
+
+            int elements = rndArray.Next(10, 25);
+            Console.WriteLine($"Array size: {elements}");
+
+            int[] array = new int[elements];
+
+            custom_Sort.ArrayRand(array, elements);
+            custom_Sort.BubbleSort(array);
+            custom_Sort.Print_Array(array);
+
+            Console.WriteLine();
+        }
+
+    }
+}
