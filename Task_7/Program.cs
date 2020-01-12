cafe8b6086547003bd1416ce08a8a07b787b1bbe
AkimovMO <akimovmi95@gmail.com>
2020-01-12 13:31:02 +0400

    Add Task 7.1

---
+++ Task_7/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Task_7.Tasks._7._1_Data_Existance;
+
+namespace Task_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Regex");
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+            do
+            {
+                Console.WriteLine("Select tasks: \n1 - Data Existance\n2 - Html Replacer\n3 - Email Finder\n4 - Number Validator\n5 - Time Counter\n0 - Exit");
+                var numberList = Console.ReadLine();
+
+                if (int.TryParse(numberList, out n))
+                {
+
+                    switch (n)
+                    {
+                        case 1:
+                            Data_Existance.StartRegex();
+                            break;
+                            
+                        case 2:
+
+                            break;
+
+                        case 3:
+
+                            break;
+
+                        case 4:
+
+                            break;
+
+                        case 5:
+
+                            break;
+
+                        default:
+                            n = 0;
+                            break;
+                    }
+
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Try again...");
+                    Console.ForegroundColor = ConsoleColor.Gray;
+
+                    n = -1;
+                }
+
+            } while (n != 0);
+        }
+    }
+}
---
+++ Task_7/Tasks/7.1 Data Existance/Data_Existance.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Task_7.Tasks._7._1_Data_Existance
+{
+    internal class Data_Existance
+    {
+
+        public static void StartRegex()
+        {
+            //[0-3][0-9]-[0|1][0-9]-(19|20)[0-9]{2}
+            Regex regex = new Regex(@"[0-3][0-9]-[0|1][0-9]-[0-9]{3,}");
+
+            Console.WriteLine("Enter text with data format dd-mm-yyyy: ");
+            var text = Console.ReadLine();
+
+            if (regex.IsMatch(text))
+            {
+                Console.WriteLine($"The {text} contains the date!");
+            }
+            else
+            {
+                Console.WriteLine($" The {text} does not contain a date!");
+            }
+
+        }
+
+    }
+}
