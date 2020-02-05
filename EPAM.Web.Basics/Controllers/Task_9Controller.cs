5c8592c4e625fa5c30c42359f2e8b7b062451d56
AkimovMO <akimovmi95@gmail.com>
2020-02-05 16:01:38 +0400

    Add Task 9.1

---
+++ EPAM.Web.Basics/Controllers/Task_9Controller.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EPAM.Web.Basics.Controllers
+{
+    public class Task_9Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Task9_1()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ EPAM.Web.Basics/wwwroot/Script/Task_9/Task_9_1Script.js
@@ -0,0 +1,51 @@
+﻿let btn = document.querySelector("#btn");
+let text = document.querySelector("#Text1").value;
+
+function remover() {
+    var words = text.split(' ');
+    var sep = ["?", "!", ":", ";", ",", "."];
+    var res = [];
+
+    for (let i = 0; i < words.length; i++)
+    {
+        if (words[i].indexOf(sep) == -1)
+        {
+            var word = words[i].split('');
+            res += wordsSplit(word).join('');
+            res += " ";
+        } 
+    }
+    document.querySelector("#Text1").value = res;
+}
+
+function wordsSplit(word)
+{
+    var remove = [];
+    remove = remove.concat(word);
+
+    for (let i = 0; i < word.length; i++)
+    {
+        for (let j = i + 1; j < word.length; j++)
+        {
+            if (word[i] == word[j] && word[i] != undefined && word[j] != undefined)
+            {
+                if (word[i] != undefined) 
+                    delete word[i];
+                if (word[j] != undefined)
+                    delete word[j];
+            }
+        }
+    }
+    word = deleteEmpty(word);
+    //console.log(word);
+    return word;
+}
+
+function deleteEmpty(word)
+{
+    let emptyDel = [];
+    emptyDel = word.filter(element => element !== undefined);
+    return emptyDel;
+}
+
+btn.onclick = remover;
