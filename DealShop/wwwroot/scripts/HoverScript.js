4cc777e43f4e88229d94eeb51f7ec487c148c590
AkimovMO <akimovmi95@gmail.com>
2020-03-15 23:01:39 +0400

    Add new style start page

---
+++ DealShop/wwwroot/scripts/HoverScript.js
@@ -0,0 +1,25 @@
+﻿const content = document.querySelector(".contenthover");
+const left = document.querySelector(".left");
+const right = document.querySelector(".right");
+
+// left
+left.addEventListener('mouseenter', () =>
+{
+    content.classList.add('hover-left');
+})
+
+left.addEventListener('mouseleave', () =>
+{
+    content.classList.remove('hover-left');
+})
+
+// right
+right.addEventListener('mouseenter', () =>
+{
+    content.classList.add('hover-right');
+})
+
+right.addEventListener('mouseleave', () =>
+{
+    content.classList.remove('hover-right');
+})
